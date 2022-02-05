e75f63e440207c6843c2069bcffc134a790f805e
santimcs <santimcs@hotmail.com>
2022-02-05 21:04:04 +0700

    22-02-05 morn

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,6 +69,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.require(:order).permit(:trade, :name, :qty, :price, :amount, :active)
+      params.require(:order).permit(:trade, :name, :qty, :price, :amount, :active, :reason, :market)
     end
 end
--- db/crt_factsheets.rb
+++ db/crt_factsheets.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class CrtFactsheets < ActiveRecord::Base
 	records = 0
-  CSV.foreach(Rails.root.join("db/factsheet.csv"), headers: false) do |row|
+  CSV.foreach(Rails.root.join("db/factsheet.csv"), headers: true) do |row|
 
       Factsheet.find_or_create_by(name: row[0], item_id: row[1], figure1: row[2],
       figure2: row[3], figure3: row[4], figure4: row[5])
