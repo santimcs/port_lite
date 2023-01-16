540ae46e6cea40fe78ae50725c60fb82eb7b7705
santimcs <santimcs@hotmail.com>
2023-01-16 23:19:47 +0700

    23-01-16 02

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -36,122 +36,128 @@ class Sale < ActiveRecord::Base
         end
     end
 	
-		def num_of_spread(minp,maxp)
-    div = 0.0
-    gap = 0.0
-    spd = 0
-    if (maxp > minp) 
-    	gap = maxp - minp
-    	mult = 1
-    else
-    	tmpp = minp
-    	minp = maxp
-    	maxp = tmpp
-    	gap  = maxp - minp
-    	mult = -1
-    end
-    if gap != 0
-    case maxp
-    	when 0.00..2.00 then 
-        begin
-            div = 0.01 
-            spd = gap/div
-        end
-    	when 2.02..5.00 then 
-        begin
-    		div = 0.02
-    		if minp < (div * 100)
-    			spd = ((div * 100) - minp) / 0.01
-    			spd += (maxp - (div * 100)) / div
-    		else
-    			spd = gap/div
-            end
-    	end      
-    	when 5.05..10.00 then  
-        begin
-    		div = 0.05
-    		if minp < (div * 100)
-    			spd = ((div * 100) - minp) / 0.02
-    			spd += (maxp - (div * 100)) / div
-    		else
-    			spd = gap/div
-           end            
-    	end      
-    	when 10.10..25.00 then 
-    	begin
-    		div = 0.10
-    		if minp < (div * 100)
-    			spd = ((div * 100) - minp) / 0.05
-    			spd += (maxp - (div * 100)) / div
-    		else
-    			spd = gap/div
-           end          
-    	end   
-    	when 25.25..100.00 then 
-    	begin
-    		div = 0.25
-    		if minp < (div * 100)
-    			spd = ((div * 100) - minp) / 0.10
-    			spd += (maxp - (div * 100)) / div
-    		else
-    			spd = gap/div
-           end            
-    	end    
-    	when 100.50..200.00 then 
-    	begin
-    		div = 0.50
-    		if minp < (div * 100)
-    			spd = ((div * 100) - minp) / 0.25
-    			spd += (maxp - (div * 100)) / div
-    		else
-    			spd = gap/div
-            end             
-    	end      
-    	when 201.00..400.00 then 
-    	begin
-    		div = 1.00
-    		if minp < (div * 200)
-    			spd = ((div * 200) - minp) / 0.50
-    			spd += (maxp - (div * 200)) / div
-    		else
-    			spd = gap/div
-           end            
-    	end   
-    	else 
-    	begin
-    		div = 2.00
-    		if minp < (div * 200)
-    			spd = ((div * 200) - minp) / 1.00
-    			spd += (maxp - (div * 200)) / div
-    		else
-    			spd = gap/div
-           end            
-    	end         
-    end	
+	def num_of_spread(minp,maxp)
+		div = 0.0
+		gap = 0.0
+		spd = 0
+
+		if (maxp > minp) 
+			gap = maxp - minp
+			mult = 1
+		else
+			tmpp = minp
+			minp = maxp
+			maxp = tmpp
+			gap  = maxp - minp
+			mult = -1
+		end
+
+		if gap != 0
+			case maxp
+				when 0.00..2.00 then 
+				begin
+					div = 0.01 
+					spd = gap/div
+				end
+				when 2.02..5.00 then 
+				begin
+					div = 0.02
+					if minp < (div * 100)
+						spd = ((div * 100) - minp) / 0.01
+						spd += (maxp - (div * 100)) / div
+					else
+						spd = gap/div
+					end
+				end      
+				when 5.05..10.00 then  
+				begin
+					div = 0.05
+					if minp < (div * 100)
+						spd = ((div * 100) - minp) / 0.02
+						spd += (maxp - (div * 100)) / div
+					else
+						spd = gap/div
+					end            
+				end      
+				when 10.10..25.00 then 
+				begin
+					div = 0.10
+					if minp < (div * 100)
+						spd = ((div * 100) - minp) / 0.05
+						spd += (maxp - (div * 100)) / div
+					else
+						spd = gap/div
+					end          
+				end   
+				when 25.25..100.00 then 
+				begin
+					div = 0.25
+					if minp < (div * 100)
+						spd = ((div * 100) - minp) / 0.10
+						spd += (maxp - (div * 100)) / div
+					else
+						spd = gap/div
+					end            
+				end    
+				when 100.50..200.00 then 
+				begin
+					div = 0.50
+					if minp < (div * 100)
+						spd = ((div * 100) - minp) / 0.25
+						spd += (maxp - (div * 100)) / div
+					else
+						spd = gap/div
+					end             
+				end      
+				when 201.00..400.00 then 
+				begin
+					div = 1.00
+					if minp < (div * 200)
+						spd = ((div * 200) - minp) / 0.50
+						spd += (maxp - (div * 200)) / div
+					else
+						spd = gap/div
+					end            
+				end   
+				else 
+				begin
+					div = 2.00
+					if minp < (div * 200)
+						spd = ((div * 200) - minp) / 1.00
+						spd += (maxp - (div * 200)) / div
+					else
+						spd = gap/div
+					end            
+				end         
+		end	
+		end
+		return spd * mult
 	end
-	return spd * mult
-end
 
 	def assign_values
 		# days
 		self.days = 0
 		d1 = self.fm_date
 		d2 = self.to_date
+
 		while (d1 < d2)
 			d1 = d1 + 1
 			if d1.cwday < 6
 				self.days = self.days + 1
 			end
 		end
+
 		# diff
 		self.diff = self.to_price - self.fm_price
 		self.pct = self.diff/self.fm_price*100
 		self.ttl_spread = num_of_spread(self.fm_price,self.to_price)
+		
         if self.days != 0
-		  self.avg_spread = (self.ttl_spread / self.days).round
+		  	self.avg_spread = (self.ttl_spread / self.days).round
         else
             self.avg_spread = 0
         end
+
         if (self.to_price > self.max_price)
             self.max_price = self.to_price
         end
